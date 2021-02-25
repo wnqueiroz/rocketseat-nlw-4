3b71ba2fa321db26edb646e8faea1376fd9ab664
William Queiroz <wnqueiroz@outlook.com>
2021-02-25 18:55:03 -0300

    feat: using ContextAPI

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
 import styles from '../styles/components/Countdown.module.css'
 
 let countdownTimeout: NodeJS.Timeout
 
 export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext)
+
   const initialTime = 25 * 60
 
   const [time, setTime] = useState(initialTime)
@@ -34,6 +38,7 @@ export function Countdown() {
     } else if (isActive && time === 0) {
       setHasFinished(true)
       setIsActive(false)
+      startNewChallenge()
     }
   }, [isActive, time])
 
